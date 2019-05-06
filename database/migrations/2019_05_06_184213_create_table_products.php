93cde7114a5c738fafdf79727877b46faab096a5
Igor-Nosatov <sinevor13@gmail.com>
2019-05-06 22:23:57 +0300

    add product migration

---
+++ database/migrations/2019_05_06_184213_create_table_products.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableProducts extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('price');
+            $table->text('description');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('=products');
+    }
+}
